ee982e63390955956fb57d63944509dd48492115
onifernando1 <onifernando1@gmail.com>
2023-05-24 20:49:27 +0100

    form onsubmit save to state

--- cv-project/src/components/General.js
+++ cv-project/src/components/General.js
@@ -3,19 +3,30 @@ import React, { Component } from "react";
 class General extends React.Component {
   constructor() {
     super();
+
+    this.state = {
+      name: "",
+      email: "",
+    };
   }
+
+  onSubmitGeneral = (e) => {
+    e.preventDefault();
+    this.setState({
+      name: e.target[0].value,
+      email: e.target[1].value,
+    });
+  };
+
   render() {
     return (
       <div>
-        <form>
+        <form onSubmit={this.onSubmitGeneral}>
           <label htmlFor="name">Name</label>
           <input type="text" id="name"></input>
 
           <label htmlFor="email">Email</label>
           <input type="email" id="email"></input>
-
-          <label htmlFor="password">Name</label>
-          <input type="password" id="password"></input>
 
           <button type="submit">Submit</button>
         </form>
